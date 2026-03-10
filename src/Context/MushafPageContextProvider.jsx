f647016b0ad642ddb7874db16cf0d9221d18ba8b
Mahmud <rainbowmahmud@gmail.com>
2026-03-10 09:51:20 +0600

    data structure completed. implementation needed

--- src/Context/MushafPageContextProvider.jsx
+++ src/Context/MushafPageContextProvider.jsx
@@ -1,21 +1,18 @@
 import { createContext, useContext } from "react";
 import { useParams } from "react-router-dom";
-import { SuraContext } from "./SuraContextProvider"
 import { parseVerseKey } from "../Helper/pageBuilder"
 import quranPages from "../assets/data/quranByPage.json"
+import verseKeyToPage from "../assets/data/suraAyaToPage.json"
 
 export const MushafPageContext = createContext();
 
 const MushafPageContextProvider = ({ children }) => {
-  const { sura } = useContext(SuraContext)
   const { suraId: verseKey } = useParams(); // 2 or 2:20
 
-  const { ayahId } = parseVerseKey(verseKey);
+  const { surahId, ayahId } = parseVerseKey(verseKey);
 
-  const pageNumber = sura.aya[ayahId - 1]["page"];
+  const pageNumber = verseKeyToPage[`${surahId}:${ayahId}`];
   const pageAyahs = quranPages[pageNumber]
-
-  
 
   return (
     <MushafPageContext.Provider
